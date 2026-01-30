c2c255191a24a84aee37301c6f709e3a67d2b654
waitlangpar <germodenmhar@gmail.com>
2026-01-30 21:53:35 +0800

    wala

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,14 +1,8 @@
 import { Image } from "expo-image";
 import { Platform, StyleSheet } from "react-native";
 
-// import { HelloWave } from '@/components/hello-wave';
-// import ParallaxScrollView from '@/components/parallax-scroll-view';
-// import { ThemedText } from '@/components/themed-text';
-// import { ThemedView } from '@/components/themed-view';
-// import { Link } from 'expo-router';
-
 export default function HomeScreen() {
-  return <>dsHomescreen</>;
+
 }
 
 const styles = StyleSheet.create({
